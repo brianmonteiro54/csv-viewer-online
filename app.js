acdddf71e6088bf72a331894dd76a4e0edb8ef99
brianmonteiro54 <brianmonteiro54@yahoo.com.br>
2024-04-27 21:29:35 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+function capitalizeWords(name) {
+    return name.split(' ').map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+}
+
 document.getElementById('csvFileInput').addEventListener('change', function(event) {
     const file = event.target.files[0];
     if (file) {
@@ -12,24 +16,18 @@ document.getElementById('csvFileInput').addEventListener('change', function(even
 
 function processContent(content) {
     const lines = content.split(/\r?\n/);
-    const uniqueNames = new Set();
     const groupedByInitial = {};
 
     lines.slice(10).forEach(line => {
-        let name = line.split('\t')[0].trim(); // Assumindo que o nome está na primeira coluna
-        name = name.replace(/["']+/g, '').replace(/\(Não verificado\)/, '').trim();
-        name = name.split(' ').map(w => w.charAt(0).toUpperCase() + w.slice(1).toLowerCase()).join(' '); // Capitaliza cada palavra do nome
+        let name = line.split('\t')[0].replace(/["']+/g, '').replace(/\(Não verificado\)/, '').trim();
+        name = capitalizeWords(name); // Capitaliza cada palavra do nome
         if (name && name !== 'Nome' && !name.startsWith('3.')) {
-            uniqueNames.add(name);
+            const initial = name[0].toUpperCase();
+            if (!groupedByInitial[initial]) {
+                groupedByInitial[initial] = new Set(); // Usar um Set para armazenar os nomes e evitar duplicatas
+            }
+            groupedByInitial[initial].add(name);
         }
-    });
-
-    Array.from(uniqueNames).sort().forEach(name => {
-        const initial = name[0].toUpperCase();
-        if (!groupedByInitial[initial]) {
-            groupedByInitial[initial] = [];
-        }
-        groupedByInitial[initial].push(name);
     });
 
     displayData(groupedByInitial);
@@ -42,7 +40,7 @@ function displayData(groupedByInitial) {
     tableHtml += '<thead><tr><th>Inicial</th><th>Nome</th></tr></thead><tbody>'; // Adicionar cabeçalhos da tabela
 
     Object.keys(groupedByInitial).sort().forEach(initial => {
-        groupedByInitial[initial].forEach(name => {
+        Array.from(groupedByInitial[initial]).sort((a, b) => a.localeCompare(b)).forEach(name => {
             tableHtml += `<tr><td>${initial}</td><td>${name}</td></tr>`; // Adicionar linhas com inicial e nome
         });
     });
